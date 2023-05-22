aa02d2c4c008d6231775139eba605dba10302f00
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-05-22 23:25:24 +0200

    added wind speed and FontAwesome Icons

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,27 @@
-// Weather.js
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchWeather } from '../redux/Actions';
 import './weather.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloud, faSun, faBolt, faCloudRain, faSnowflake, faSmog, faWind, faWater, faCloudShowersHeavy } from '@fortawesome/free-solid-svg-icons';
+
+const weatherIcons = {
+  "Thunderstorm": faBolt,
+  "Drizzle": faCloudRain,
+  "Rain": faCloudShowersHeavy,
+  "Snow": faSnowflake,
+  "Mist": faSmog,
+  "Smoke": faSmog,
+  "Haze": faSmog,
+  "Dust": faSmog,
+  "Fog": faSmog,
+  "Sand": faSmog,
+  "Ash": faSmog,
+  "Squall": faWind,
+  "Tornado": faWind,
+  "Clear": faSun,
+  "Clouds": faCloud,
+};
 
 const Weather = ({ city }) => {
   const dispatch = useDispatch();
@@ -26,12 +45,15 @@ const Weather = ({ city }) => {
     return null;
   }
 
+  const icon = weatherIcons[weatherData.weather[0].main];
+
   return (
     <div className="weatherCard">
       <h2 className="weatherTitle">{city}</h2>
       <p className="weatherInfo">Temperature: {convertToCelsius(weatherData.main.temp)}°C</p>
       <p className="weatherInfo">Weather: {weatherData.weather[0].description}</p>
-      <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="weather icon" />
+      <p className="weatherInfo">Wind Speed: {weatherData.wind.speed} m/s <FontAwesomeIcon icon={faWind} /></p>
+      {icon && <FontAwesomeIcon icon={icon} size="2x" />}
     </div>
   );
 };
